c734e60c534d30fccd7d50079528fe54fbf5da72
Daniel Ring <dbr787@gmail.com>
2024-06-12 22:43:20 +1000

    update text onresize and layout vertical

--- components/ResizableRectangle2.tsx
+++ components/ResizableRectangle2.tsx
@@ -9,21 +9,22 @@ const ResizableRectangle: React.FC = () => {
   const [dimensions, setDimensions] = useState({ width: 256, height: 256 });
 
   return (
-    <div className="p-8 flex justify-center space-x-4">
+    <div className="p-8 flex flex-col items-center space-y-4">
       <div className="border rounded-md p-6 border-solid bg-white shadow-md w-[320px] h-[320px] flex items-center justify-center">
         <ResizableBox
           width={dimensions.width}
           height={dimensions.height}
           minConstraints={[4, 4]}
           maxConstraints={[256, 256]}
-          onResizeStop={(event, { size }) =>
+          onResize={(event, { size }) =>
             setDimensions({ width: size.width, height: size.height })
           }
           resizeHandles={["s", "e", "w", "n", "se", "sw", "ne", "nw"]}
           className="bg-blue-400"
+          draggableOpts={{ grid: [1, 1] }}
         ></ResizableBox>
       </div>
-      <div className="border rounded-md p-6 border-solid bg-white shadow-md w-[320px] h-[320px] flex items-center justify-center text-center text-xs font-mono">
+      <div className="border rounded-md p-6 border-solid bg-white shadow-md w-[320px] flex items-center justify-center text-center text-xs font-mono">
         Width: {dimensions.width}px, Height: {dimensions.height}px
       </div>
     </div>
