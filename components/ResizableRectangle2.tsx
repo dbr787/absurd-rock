d2821ca3567d8821456f0f393990848c09add9f9
Daniel Ring <dbr787@gmail.com>
2024-06-12 22:53:10 +1000

    fix resize to 1px interval

--- components/ResizableRectangle2.tsx
+++ components/ResizableRectangle2.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import React, { useState } from "react";
-import { ResizableBox } from "react-resizable";
+import { ResizableBox, ResizableBoxProps } from "react-resizable";
 import "react-resizable/css/styles.css";
 import "tailwindcss/tailwind.css";
 
 const ResizableRectangle: React.FC = () => {
   const [dimensions, setDimensions] = useState({ width: 256, height: 256 });
+
+  const onResize: ResizableBoxProps["onResize"] = (event, { size }) => {
+    const width = Math.round(size.width);
+    const height = Math.round(size.height);
+    setDimensions({ width, height });
+  };
 
   return (
     <div className="p-8 flex flex-col items-center space-y-4">
@@ -16,12 +22,9 @@ const ResizableRectangle: React.FC = () => {
           height={dimensions.height}
           minConstraints={[4, 4]}
           maxConstraints={[256, 256]}
-          onResize={(event, { size }) =>
-            setDimensions({ width: size.width, height: size.height })
-          }
+          onResize={onResize}
           resizeHandles={["s", "e", "w", "n", "se", "sw", "ne", "nw"]}
           className="bg-blue-400"
-          draggableOpts={{ grid: [1, 1] }}
         ></ResizableBox>
       </div>
       <div className="border rounded-md p-6 border-solid bg-white shadow-md w-[320px] flex items-center justify-center text-center text-xs font-mono">
