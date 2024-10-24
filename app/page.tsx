8a99c54c1c98b9f90bb2db68499cd6cc96f47695
Daniel Ring <dbr787@gmail.com>
2024-10-24 13:12:46 +1100

    make all visible on page for design team

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,22 @@ import { RectangleRadiusForm } from "../components/RectangleRadiusForm";
 import ResizableRectangle from "../components/ResizableRectangle";
 import ResizableRectangle2 from "../components/ResizableRectangle2";
 
-// export default function Home() {
-//   return (
-//     <div className="p-16 flex justify-center">
-//       <div className="border rounded-md p-6 border-solid bg-white shadow-md">
-//         {/* <RectangleRadiusForm /> */}
-//         {/* <ResizableRectangle initialWidth={100} initialHeight={100} /> */}
-//         <div className="w-[512px] h-[512px]">
-//           <ResizableRectangle2 />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
 export default function Home() {
-  return <ResizableRectangle2 />;
+  return (
+    <div className="p-16 flex flex-col justify-center items-center">
+      <div className="border rounded-md p-6 border-solid bg-white shadow-md">
+        <RectangleRadiusForm />
+        {/* <ResizableRectangle
+          initialWidth={100}
+          initialHeight={100}
+        /> */}
+        <div className="">
+          <ResizableRectangle2 />
+        </div>
+      </div>
+    </div>
+  );
 }
+// export default function Home() {
+//   return <ResizableRectangle2 />;
+// }
